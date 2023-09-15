2e98e3981de879332a6e518c04a697aeee0cff27
Yakshesh Gupta <yaksheshgupta59@gmail.com>
2023-09-16 03:58:15 +0530

    ERC1155 factory

--- contracts/contracts/TokenFactory1155.sol
+++ contracts/contracts/TokenFactory1155.sol
@@ -2,48 +2,70 @@
 pragma solidity ^0.8.2;
 import "./ERC1155Token.sol";
 
-
 contract TokenFactory {
-    
-    event NewTokenCreated(string name, string symbol, uint256 amount);
+    event New1155Created(
+        uint256 id,
+        uint256[] tokenId,
+        string[] tokenName,
+        uint256[] tokenAmount,
+        bytes[] tokenData
+    );
 
     uint numOfProjects;
 
-    struct projectMeta{
+    struct projectMeta {
         uint256 id;
         string projectName;
-        string symbol;
-        uint256 initialSupply;
+        // string symbol;
+        uint256[] tokenId;
+        string[] tokenName;
+        uint256[] initialSupply;
         address tokenBeneficiary;
         address contractAddress;
     }
 
     // projectMeta[] public deployedProjects;
     mapping(uint => projectMeta) private deployedProjects;
-    
-    function createProject(string memory name, string memory symbol, uint initialSupply, address beneficiary) public {
+
+    function createProject(
+        uint256[] memory _id,
+        string memory projectName,
+        string[] memory _name,
+        // string memory symbol,
+        uint256[] memory _amount,
+        bytes[] memory _data,
+        address beneficiary
+    ) public {
         uint projectId = numOfProjects++;
         projectMeta storage project = deployedProjects[projectId];
         project.id = projectId;
-        project.projectName = name;
-        project.symbol = symbol;
-        project.initialSupply = initialSupply;
+        project.projectName = projectName;
+        // project.symbol = symbol;
+        project.initialSupply = _amount;
         project.tokenBeneficiary = beneficiary;
-        project.contractAddress = address(new ERC20Token(name, symbol, initialSupply,  beneficiary));
-        emit NewTokenCreated(name, symbol, initialSupply);
+        project.contractAddress = address(
+            new ERC1155_Equistart_V1(_id, _name, _amount, _data)
+        );
+        emit New1155Created(projectId,_id, _name, _amount,_data);
         // deployedProjects.push(newProject);
     }
-    
-    function getAllDeployedProjects () public view returns (projectMeta[] memory props){
+
+    function getAllDeployedProjects()
+        public
+        view
+        returns (projectMeta[] memory props)
+    {
         // return deployedProjects;
         props = new projectMeta[](numOfProjects);
-        
-        for (uint256 index =0; index< numOfProjects; index++){
+
+        for (uint256 index = 0; index < numOfProjects; index++) {
             props[index] = deployedProjects[index];
         }
     }
 
-    function getProjectById(uint256 projectId) public view returns (projectMeta memory){
+    function getProjectById(
+        uint256 projectId
+    ) public view returns (projectMeta memory) {
         return deployedProjects[projectId];
     }
 }
